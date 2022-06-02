76051527b0e9b1505a256ba4a2a48dde0658619c
Jan Henseler <jan.f.henseler@gmail.com>
2022-06-02 12:48:37 +0200

    .

--- WakeApp/src/UIDatabaseConnector.java
+++ WakeApp/src/UIDatabaseConnector.java
@@ -1,16 +1,45 @@
 //TODO: getter und setter für alle Felder der UI
 public class UIDatabaseConnector {
 	
+	public String ankunftszeit;
+	public String fertigmachZeit;
 	public String wohnort;
-	public String ankuftszeit;
+	public String zielort;
+	public boolean oepnv;
+	public boolean auto;
+	public boolean fahrrad;
+	
 	//Getter
+	public String getAnkunftszeit() {
+		return ankunftszeit;
+	}
+	
+	public String getFertigmachZeit() {
+		return fertigmachZeit;
+	}
 	public String getWohnort() {
 		return wohnort;
 	}
 	
+	public String getZielort() {
+		return zielort;
+	}
+	
 	//Setter
+	public void setAnkunftszeit(String newAnkunftszeit) {
+		this.ankunftszeit = newAnkunftszeit;
+	}
+	
+	public void setFertigmachZeit(String newFertigmachZeit) {
+		this.fertigmachZeit = newFertigmachZeit;
+	}
+	
 	public void setWohnort(String newWohnort) {
 		this.wohnort = newWohnort;
+	}
+	
+	public void setZielort(String newZielort) {
+		this.zielort= newZielort;
 	}
 
 }
