6399985b7455b8bd54969cc795c33cb84805b0de
Jan Henseler <jan.f.henseler@gmail.com>
2022-06-02 11:51:57 +0200

    addad uidatabaseconnector

---
+++ WakeApp/src/UIDatabaseConnector.java
@@ -0,0 +1,10 @@
+//TODO: getter und setter für alle Felder der UI
+public class UIDatabaseConnector {
+	
+	public String wohnort;
+	//Getter
+	public String getWohnort() {
+		return wohnort;
+	}
+
+}
