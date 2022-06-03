0514c45d6904c8284e3ba34c90379e3228cc5c49
Jan Henseler <jan.f.henseler@gmail.com>
2022-06-03 12:51:40 +0200

    l

--- WakeApp/src/UIDatabaseConnector/DataStorage.java
+++ WakeApp/src/UIDatabaseConnector/DataStorage.java
@@ -1,13 +1,34 @@
 package UIDatabaseConnector;
 
+import java.io.IOException;
+import java.io.*;
+
+
 public class DataStorage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-	}
-	public boolean speichern() {
-		return true;
-	}
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+
+
+File file = new File("data.txt"); //Datei, in die geschrieben werden soll
+
+try {
+BufferedWriter writer = new BufferedWriter(new FileWriter(file)); //Erzeugen eines effizienten Writers für Textdateien
+writer.write("Test");
+writer.newLine();
+writer.write("Test");
+writer.newLine();
+writer.write("Test");
+writer.newLine();
+writer.write("Test1");
+writer.close();
+}
+catch(IOException ioe) {
+System.err.println(ioe);
+}
+
+}
+public boolean speichern() {
+return true;
+}
 }
